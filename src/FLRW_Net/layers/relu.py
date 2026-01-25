c8e1a0a23ef677186a535717539d508ae8d0beb7
Hilpertspace <82518583+Hilpertspace@users.noreply.github.com>
2026-01-25 18:26:15 +0100

    Added ReLU layer to the neural network

---
+++ src/FLRW_Net/layers/relu.py
@@ -0,0 +1,35 @@
+"""Layer for application of ReLU to all elements except the ones for the boundary."""
+import tensorflow as tf
+
+
+class PartialReLU(tf.keras.layers.Layer):
+    """Layer for application of ReLU to all elements except the ones for the boundary."""
+
+    def __init__(self) -> None:
+        """Initialize the layer."""
+        super().__init__()
+        self._minimum_length = 2
+
+    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        """Apply ReLU to all elements except the boundaries.
+
+        Args:
+            inputs: tf.Tensor of shape (features,) or (1, features)
+
+        Returns:
+            tf.Tensor of same shape, with ReLU applied to middle elements.
+        """
+        n = tf.shape(inputs)[1]
+
+        def too_short() -> tf.Tensor:
+            """Return tensor as-is."""
+            return inputs
+
+        def relu_middle() -> tf.Tensor:
+            first = tf.cast(inputs[:, 0:1], tf.float64)
+            last = tf.cast(inputs[:, -1:], tf.float64)
+
+            middle = tf.nn.relu(inputs[:, 1:-1])
+            return tf.concat([first, middle, last], axis=1)
+
+        return tf.cond(n <= self._minimum_length, too_short, relu_middle)
--- src/FLRW_Net/networks/test.py
+++ src/FLRW_Net/networks/test.py
@@ -1,7 +1,9 @@
 """Minimum example for a generalized model."""
 
 import tensorflow as tf
+
 from FLRW_Net.layers.matmul import Matmul
+from FLRW_Net.layers.relu import PartialReLU
 
 
 class GeneralizedModel(tf.keras.Model):
@@ -11,10 +13,14 @@ class GeneralizedModel(tf.keras.Model):
         """Initialize the model."""
         super().__init__()
         self.slice_layers = [Matmul(start, end) for start, end in slice_specs]
+        self.relu_layer = PartialReLU()
 
     @tf.function
     def call(self, inputs: tf.Tensor) -> tf.Tensor:
         """Call the model."""
-        outputs = [layer(inputs) for layer in self.slice_layers]
+        outputs_1 = [layer(inputs) for layer in self.slice_layers]
+        outputs_1 = tf.concat(outputs_1, axis=1)
+
+        outputs = self.relu_layer(outputs_1)
 
         return tf.concat(outputs, axis=-1)
