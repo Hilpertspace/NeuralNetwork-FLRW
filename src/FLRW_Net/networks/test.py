a413f8c16d6c4d227ff47e97c3696eb7c1999828
Hilpertspace <82518583+Hilpertspace@users.noreply.github.com>
2026-01-27 23:22:27 +0100

    WIP - Added training loop

--- src/FLRW_Net/networks/test.py
+++ src/FLRW_Net/networks/test.py
@@ -1,6 +1,7 @@
 """Minimum example for a generalized model."""
 
 import tensorflow as tf
+import tqdm
 
 from FLRW_Net.layers.assembly import Assembly
 from FLRW_Net.layers.matmul import Matmul
@@ -22,6 +23,8 @@ class GeneralizedModel(tf.keras.Model):
         self.spatial_edge_activation = SpatialEdgeActivation()
         self.assembly = Assembly()
 
+        self.loss_threshold = 1e-5
+
     @tf.function
     def call(self, inputs: tf.Tensor) -> tf.Tensor:
         """Call the model."""
@@ -41,25 +44,52 @@ class GeneralizedModel(tf.keras.Model):
         return outputs_6
 
     @tf.function
-    def custom_train_step(self, inputs: tf.Tensor) -> tf.Tensor:
+    def _custom_train_step(self, inputs: tf.Tensor) -> tf.Tensor:
         """Define a single step of training."""
         with tf.GradientTape(persistent=True) as tape:
             prediction = self(inputs, training=True)
 
             loss_struts = strut_losses(prediction)
             loss_spatial_edges = spatial_edge_losses(prediction)
-
-            loss_edge_1 = self.EOM_edges(prediction[:, 0:5])
-            loss_edge_2 = self.EOM_edges(prediction[:, 2:7])
-
-            loss = (loss_strut_1 + loss_strut_2 + loss_strut_3 + loss_edge_1 + loss_edge_2) / tf.constant(5, dtype=tf.float64)
+            combined = tf.concat([loss_struts, loss_spatial_edges], axis=1)
+            loss = tf.reduce_mean(combined, axis=1)
 
         # Tell tensorflows automatic gradient computation to compute the gradients
-        # of the loss with respect to the trainable variables of the network:
-        # here only the three weights of the strut-neuron in the hidden layer
-        gradients = tape.gradient(loss, self.trainable_variables)
+        # of the loss with respect to the trainable variables of the network.
+        gradients = tape.gradient(loss, self.slice_layers.trainable_variables)
 
         # Apply the gradients to update the weights
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
 
-        return {"loss": loss}
+        return loss
+
+    def training(self, inputs: tf.Tensor, epochs: int) -> tuple[tf.Tensor, list[tf.Tensor]]:
+        """Training logic of the neural network.
+
+        The train_step function is called iteratively until the specified number of epochs is reached.
+        """
+        minimum_loss = tf.Variable(tf.float32.max, trainable=False)
+        loss_history = []
+        best_weights = [tf.Variable(w, trainable=False) for w in self.trainable_variables]
+
+        for _ in tqdm(range(epochs), desc="Training", unit="step", ncols=70): # pyright: ignore[reportCallIssue]
+            loss = self._custom_train_step(inputs)
+            loss_value = float(loss)
+            loss_history.append(loss_value)
+
+            # Update best weights
+            if loss_value < float(minimum_loss):
+                minimum_loss.assign(loss_value)
+                for bw, w in zip(best_weights, self.trainable_variables):
+                    bw.assign(w)
+
+            # Early stopping
+            if loss_value < self.loss_threshold:
+                print(f"\n--- Loss below threshold ({self.loss_threshold}). Stopping. ---")
+                break
+
+        # Restore best weights
+        for w, bw in zip(self.trainable_variables, best_weights):
+            w.assign(bw)
+
+        return float(minimum_loss), loss_history
