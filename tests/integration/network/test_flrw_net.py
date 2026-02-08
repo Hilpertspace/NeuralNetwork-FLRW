bdb41a27311d067cd06b56858eecd219a36fb37e
Hilpertspace <82518583+Hilpertspace@users.noreply.github.com>
2026-02-08 13:30:16 +0100

    Added tests for training

--- tests/integration/network/test_flrw_net.py
+++ tests/integration/network/test_flrw_net.py
@@ -77,7 +77,8 @@ def setup_training_test() -> dict:
     }
 
     return {
-
+        "inputs": inputs,
+        "outputs": outputs,
     }
 
 
@@ -103,5 +104,57 @@ def test_forward_feeding(number_of_timesteps: int, string_name: str, setup_forwa
         setup_forward_feed_test["outputs"][string_name],
     )
 
+@pytest.mark.parametrize(
+    ("number_of_timesteps", "string_name", "triangulation"),
+    [
+        (1, "one", "5-cell"),
+        (2, "two", "5-cell"),
+        (3, "three", "5-cell"),
+        # (4, "four", "5-cell"),
+        (1, "one", "16-cell"),
+        (2, "two", "16-cell"),
+        (3, "three", "16-cell"),
+        # (4, "four", "16-cell"),
+        (1, "one", "600-cell"),
+        (2, "two", "600-cell"),
+        # (3, "three", "600-cell"),
+        # (4, "four", "600-cell"),
+    ]
+)
+def test_training(number_of_timesteps: int, string_name: str, triangulation: str, setup_training_test: dict) -> None:
+    if number_of_timesteps == 1:
+        cosmological_constant = 1e-3
+        clipnorm = 1
+        lr = 0.001 if triangulation != "600-cell" else 0.1
+    else:
+        cosmological_constant = 1e-3
+        clipnorm = 2
+        lr = 1e-6 if triangulation != "600-cell" else 0.01
 
-def test_training() -> None:
+    tf.keras.backend.set_floatx("float64")
+    flrw_net = NeuralNetwork(
+        number_of_timesteps=number_of_timesteps,
+        triangulation=triangulation,
+        cosmological_constant=cosmological_constant,
+    )
+    adam_optimizer = tf.keras.optimizers.Adam(
+        learning_rate=lr,
+        beta_1=0.9,
+        beta_2=0.999,
+        epsilon=1e-7,
+        amsgrad=False,
+        decay=0.9,
+        clipnorm=clipnorm,
+        clipvalue=None,
+        global_clipnorm=None,
+    )
+    flrw_net.compile(optimizer=adam_optimizer)
+
+    flrw_net.training(setup_training_test["inputs"][triangulation][string_name], epochs=5000)
+
+    tf.debugging.assert_near(
+        flrw_net(setup_training_test["inputs"][triangulation][string_name]),
+        setup_training_test["outputs"][triangulation][string_name],
+        rtol=1e-6,
+        atol=1e-10,
+    )
